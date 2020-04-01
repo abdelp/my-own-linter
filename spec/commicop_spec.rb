0dd6469c1629d5053628572ac5a15c7029a83ba3
abdelp <juniorperezpy@gmail.com>
2020-04-01 07:51:47 -0400

    Add #methods_to_check rspec validation test

--- spec/commicop_spec.rb
+++ spec/commicop_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../lib/commicop.rb'
 
 RSpec.describe Commicop do
-    
+    let(:branch) { 'develop' }
+    let(:commicop) { Commicop.new(branch) }
+    let(:methods) { [{:method=>"capitalized_subject", :params=>{"Enabled"=>true}},
+         {:method=>"subject_length", :params=>{"Max"=>50}},
+         {:method=>"body_length", :params=>{"Min"=>10}},
+         {:method=>"imperative_subject", :params=>{"Enabled"=>true}},
+         {:method=>"body_required", :params=>{"Enabled"=>true}}, 
+         {:method=>"valid_grammar", :params=>{"Enabled"=>true}}
+        ]}
+
+    describe '#methods_to_check' do
+        it 'returns the methods not having the attribute Enable: false' do
+            expect(commicop.methods_to_check).to eq(methods)
+        end
+    end
 end
